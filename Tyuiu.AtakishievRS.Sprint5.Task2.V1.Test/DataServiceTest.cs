784b1dfba849ab68c1b43424276e87d19bde8469
AtakishievRashid <Tyuiu.AtakishievRS@outlook.com>
2025-11-29 00:40:29 +0500

    Task2

---
+++ Tyuiu.AtakishievRS.Sprint5.Task2.V1.Test/DataServiceTest.cs
@@ -0,0 +1,18 @@
+﻿using Tyuiu.AtakishievRS.Sprint5.Task2.V1.Lib;
+namespace Tyuiu.AtakishievRS.Sprint5.Task2.V1.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            string path = @"C:\Users\bogra\Desktop\ПРОГАВУЗТИУ\Tyuiu.AtakishievRS.Sprint5\Tyuiu.AtakishievRS.Sprint5.Task2.V1\bin\Debug\OutPutFileTask2.txt";
+
+            FileInfo fileInfo = new FileInfo(path);
+            bool fileExists = fileInfo.Exists;
+            bool wait = false;
+            Assert.AreEqual(wait, fileExists);
+        }
+    }
+}
