01e0d6cad62444f787e0148de4af8e3ac10fd3d1
AtakishievRashid <Tyuiu.AtakishievRS@outlook.com>
2025-11-29 00:52:39 +0500

    Task3

---
+++ Tyuiu.AtakishievRS.Sprint5.Task3.V8.Test/DataServiceTest.cs
@@ -0,0 +1,18 @@
+﻿using Tyuiu.AtakishievRS.Sprint5.Task3.V8.Lib;
+namespace Tyuiu.AtakishievRS.Sprint5.Task3.V8.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            string path = @"C:\Users\bogra\Desktop\ПРОГАВУЗТИУ\Tyuiu.AtakishievRS.Sprint5\Tyuiu.AtakishievRS.Sprint5.Task3.V8\bin\Debug\OutPutFileTask3.txt";
+
+            FileInfo fileInfo = new FileInfo(path);
+            bool fileExists = fileInfo.Exists;
+            bool wait = false;
+            Assert.AreEqual(wait, fileExists);
+        }
+    }
+}
---
+++ Tyuiu.AtakishievRS.Sprint5.Task3.V8/Program.cs
@@ -0,0 +1,38 @@
+﻿using Tyuiu.AtakishievRS.Sprint5.Task3.V8.Lib;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int x = 3;
+        DataService dataService = new DataService();
+
+        Console.Title = "Спринт #5 | Выполнил: Атакишиев Р.С. | РППб-25-1";
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* Спринт #5                                                               *");
+        Console.WriteLine("* Тема: Потоковый метод записи данных в бинарный файл                     *");
+        Console.WriteLine("* Задание #3                                                              *");
+        Console.WriteLine("* Вариант #8                                                              *");
+        Console.WriteLine("* Выполнил: Атакишиев Р. С. | РППб-25-1                                   *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* УСЛОВИЕ:                                                                *");
+        Console.WriteLine("* Дано выражение (x^2+1)/(3x+4) вычислить его значение при x = 3,         *");
+        Console.WriteLine("* результат сохранить в бинарный файл OutPutFileTask3.bin и вывести       *");
+        Console.WriteLine("*                                                                         *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ                                                         *");
+        Console.WriteLine("***************************************************************************");
+
+        Console.WriteLine("x = " + x);
+
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+        Console.WriteLine("***************************************************************************");
+
+        string res = dataService.SaveToFileTextData(x);
+
+        Console.WriteLine("Файл: " + res);
+        Console.WriteLine("Создан!");
+
+        Console.ReadKey();
+    }
+}
