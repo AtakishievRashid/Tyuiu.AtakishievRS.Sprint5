dc4657965c7526876dbe87a6e990da819ccacf6a
AtakishievRashid <Tyuiu.AtakishievRS@outlook.com>
2025-11-29 01:55:20 +0500

    Task5

---
+++ Tyuiu.AtakishievRS.Sprint5.Task6.V13.Test/DataServiceTest.cs
@@ -0,0 +1,18 @@
+﻿using Tyuiu.AtakishievRS.Sprint5.Task6.V13.Lib;
+namespace Tyuiu.AtakishievRS.Sprint5.Task6.V13.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            string path = @"C:\Users\bogra\Desktop\ПРОГАВУЗТИУ\Tyuiu.AtakishievRS.Sprint5\Tyuiu.AtakishievRS.Sprint5.Task6.V13.Lib\bin\Debug\OutPutFileTask6.txt";
+
+            FileInfo fileInfo = new FileInfo(path);
+            bool fileExists = fileInfo.Exists;
+            bool wait = false;
+            Assert.AreEqual(wait, fileExists);
+        }
+    }
+}
---
+++ Tyuiu.AtakishievRS.Sprint5.Task6.V13/Program.cs
@@ -0,0 +1,14 @@
+﻿using Tyuiu.AtakishievRS.Sprint5.Task6.V13.Lib;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        DataService ds = new DataService();
+
+        string path = @"C:\DataSprint5\InPutDataFileTask6V13.txt";
+        Console.WriteLine("Данные находятся в файле: " + path);
+
+        Console.WriteLine("В строке находится " + ds.LoadFromDataFile(path) + " удвоенных букв \"с\"");
+        Console.ReadKey();
+    }
+}
