c00787b45302590f2f7b67b43c454ef9c4852c2f
AtakishievRashid <Tyuiu.AtakishievRS@outlook.com>
2025-11-29 02:07:34 +0500

    Task7

---
+++ Tyuiu.AtakishievRS.Sprint5.Task7.V23.Test/DataServiceTest.cs
@@ -0,0 +1,17 @@
+﻿namespace Tyuiu.AtakishievRS.Sprint5.Task7.V23.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            string path = @"C:\Users\bogra\Desktop\ПРОГАВУЗТИУ\Tyuiu.AtakishievRS.Sprint5\Tyuiu.AtakishievRS.Sprint5.Task7.V23.Lib\bin\Debug\OutPutFileTask7.txt";
+
+            FileInfo fileInfo = new FileInfo(path);
+            bool fileExists = fileInfo.Exists;
+            bool wait = false;
+            Assert.AreEqual(wait, fileExists);
+        }
+    }
+}
---
+++ Tyuiu.AtakishievRS.Sprint5.Task7.V23/Program.cs
@@ -0,0 +1,41 @@
+﻿using Tyuiu.AtakishievRS.Sprint5.Task7.V23.Lib;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        DataService ds = new DataService();
+
+        Console.Title = "Спринт #5 | Выполнил: Атакишиев Р.С. | РППб-25-1";
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* Спринт #5                                                               *");
+        Console.WriteLine("* Тема: Добавление в решение итоговых проектов по спринту                 *");
+        Console.WriteLine("* Задание #7                                                              *");
+        Console.WriteLine("* Вариант #23                                                             *");
+        Console.WriteLine("* Выполнил: Атакишиев Р. С. | РППб-25-1                                   *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* УСЛОВИЕ:                                                                *");
+        Console.WriteLine("* Дан файл С:\\DataSprint5\\InPutDataFileTask7V23.txt в котором есть      *");
+        Console.WriteLine("* набор символьных данных.Удалить все русские слова из файла. Полученный  *");
+        Console.WriteLine("* результат сохранить в файл OutPutDataFileTask7V23.txt.                  *");
+        Console.WriteLine("*                                                                         *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ                                                         *");
+        Console.WriteLine("***************************************************************************");
+
+        string path = $@"C:\DataSprint5\InPutDataFileTask7V23.txt";
+        string pathSaveFile = $@"C:\DataSprint5\OutPutDataFileTask7V23.txt";
+
+
+        Console.WriteLine("Данные находяться в файле: " + path);
+
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+        Console.WriteLine("***************************************************************************");
+
+        Console.WriteLine("Находится в файле : ");
+        pathSaveFile = ds.LoadDataAndSave(path);
+        Console.WriteLine(pathSaveFile);
+        Console.ReadKey();
+
+    }
+}
