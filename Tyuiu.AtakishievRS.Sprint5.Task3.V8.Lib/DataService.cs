5143ec1443185f833f402d9d723a20e8582d99dd
AtakishievRashid <Tyuiu.AtakishievRS@outlook.com>
2025-11-29 12:43:08 +0500

    Task3

--- Tyuiu.AtakishievRS.Sprint5.Task3.V8.Lib/DataService.cs
+++ Tyuiu.AtakishievRS.Sprint5.Task3.V8.Lib/DataService.cs
@@ -7,10 +7,7 @@ namespace Tyuiu.AtakishievRS.Sprint5.Task3.V8.Lib
         public string SaveToFileTextData(int x)
         {
             string path = Path.Combine(Path.GetTempPath(), "OutPutFileTask3.txt");
-
-            FileInfo fileInfo = new FileInfo(path);
-            bool fileExists = fileInfo.Exists;
-
+   
             double result = Math.Round((Math.Pow(x, 2) + 1) / (3 * x + 4), 3);
             using (BinaryWriter writer = new BinaryWriter(File.Open(path, FileMode.OpenOrCreate), Encoding.UTF8))
             {
